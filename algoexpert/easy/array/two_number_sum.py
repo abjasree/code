e9503294e418609e3201a01c4c1090cd22cc14a5
abjasree <abjasree1@gmail.com>
2023-04-30 19:00:04 +0530

    Updated two number sum solutions.

--- algoexpert/easy/array/two_number_sum.py
+++ algoexpert/easy/array/two_number_sum.py
@@ -6,3 +6,28 @@ def twoNumberSum(array, targetSum):
                 return [array[i], array[j]]
                 
     return []
+
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+    num_dict = {}
+
+    for i in array:
+        if i in num_dict.keys():
+            return [i, num_dict[i]]
+        num_dict[targetSum-i] = i
+    return []
+
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+    array.sort()
+    left = 0
+    right = len(array)-1
+    while left < right:
+        if array[left] + array[right] == targetSum:
+            return [array[left], array[right]]
+        elif array[left] + array[right] > targetSum:
+            right -= 1
+        else:
+            left += 1
+    return []
+    
